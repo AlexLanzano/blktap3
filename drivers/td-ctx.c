ca915b36a48259d26d9c4b2117e67634d221bdd8
Alex Lanzano <lanzanoa@ainfosec.com>
2018-07-03 12:06:14 -0400

    debug

--- drivers/td-ctx.c
+++ drivers/td-ctx.c
@@ -5,14 +5,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *  3. Neither the name of the copyright holder nor the names of its 
- *     contributors may be used to endorse or promote products derived from 
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
  *     this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -167,6 +167,7 @@ xenio_blkif_get_request(struct td_xenblkif * const blkif,
 
     rings = &blkif->rings;
 
+
     switch (blkif->proto) {
         case BLKIF_PROTOCOL_NATIVE:
             {
@@ -183,6 +184,7 @@ xenio_blkif_get_request(struct td_xenblkif * const blkif,
                 src = RING_GET_REQUEST(&rings->x86_32, idx);
                 if (src->operation==BLKIF_OP_DISCARD) {
                     blkif_x86_32_request_discard_t * discard_src;
+					tlog_syslog(LOG_ERR, "discard_req");
                     discard_src = (blkif_x86_32_request_discard_t *) src;
                     blkif_get_req_discard(dst, discard_src);
                 } else {
@@ -197,6 +199,7 @@ xenio_blkif_get_request(struct td_xenblkif * const blkif,
                 src = RING_GET_REQUEST(&rings->x86_64, idx);
                 if (src->operation==BLKIF_OP_DISCARD) {
                     blkif_x86_64_request_discard_t * discard_src;
+					tlog_syslog(LOG_ERR, "discard_req");
                     discard_src = (blkif_x86_64_request_discard_t *) src;
                     blkif_get_req_discard(dst, discard_src);
                 } else {
